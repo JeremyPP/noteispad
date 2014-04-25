ca2e198c680e57bcb39c52adad166d3568eec813
JeremyPP <jeremy.petit@ccc.ufcg.edu.br>
2014-04-25 20:35:25 -0300

    implementação da pagina Admin (admin.php)

---
+++ admin.php
@@ -0,0 +1,55 @@
+﻿<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"
+		"http://www.w3.org/TR/html4/strict.dtd">
+<html lang="pt-br">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+		<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,600,300' rel='stylesheet' type='text/css'>
+		<link href="style.css" type="text/css" rel="stylesheet">
+		<link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+		<title>NOT is PAD!</title>
+		<meta name="viewport" content="width=device-width, initial-scale=0.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
+		<script src="http://code.jquery.com/jquery-2.1.0.min.js"></script>
+    </head>
+        <body id="admin" style="background: #333;">
+			<a href="../notepad" style=" text-decoration: none; "><div id="openb2"><img src="back.png">Voltar</div></a>
+			<h1>ADMIN.</h1>
+			<p>
+				Essa é uma area restrita resevada para os administradores. Se voce não sabe como
+				chegou aqui, ou simplesmente não é um administrador, por favor, ignore o conteudo abaixo e feche esta pagina. 
+			</p>
+			<div id="basic-info">
+				<h6>Informações gerais</h6>
+				<div>Total de notas criadas por usuarios: <span>1542</span></div>
+				<div>Total de notas rapidas criadas: <span>2541</span></div>
+				<div>Total de notas rapidas disponiveis: <span>334</span></div>
+				<br>
+				<div>Renda atual usuarios por mes: <span>2907</span></div>
+				<div>Renda total usuarios: <span>7844</span></div>
+			</div>
+			<div id="servidor-info">
+				<h6>Servidor</h6>
+				<div id="bginfoserver">
+					<div>Memoria utilizada:</div>
+					<h3>17%</h3>
+					<h4>5.1GB de 30GB</h4>
+				</div>
+			</div>
+			<div id="usuarios-info">
+				<h6>Usuarios</h6>
+				<div>Qtd. usuarios Plano Basico:</div>
+				<h3>152</h3>
+				<br>
+				<div>Qtd. usuarios Plano Pro:</div>
+				<h3>247</h3>
+				<br>
+				<div>Qtd. usuarios Plano Premium:</div>
+				<h3>20</h3>
+				<br>
+				<div id="separausersinfo"></div>
+				<div>Total de usuarios:</div>
+				<h3 style="color: #4CAED3;background: #fff;">419</h3>
+				<br>
+			</div>
+			<div id="lista-usuarios">Baixar lista de todos os usuarios</div>
+		</body>
+</html>
