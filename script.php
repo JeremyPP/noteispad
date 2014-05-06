74043535b4b935db394d978db42ce32a9fa24502
Keith Jones <keith@pbembid.com>
2014-05-06 11:39:38 +0100

    Split exit and saved codes. Saved now displays a "Saved" screen Exit destroys the session - needs a pop-up to ask if they want to save any changes

--- script.php
+++ script.php
@@ -1,72 +1,89 @@
 ﻿<?php 
-    require_once("init.php");
-    require_once("functions.php");
-    $SERVER = "http://localhost:8888";
+require_once("init.php");
+require_once("functions.php");
+$SERVER = "http://localhost:8888";
 
-	if(isset($_POST['code']))
-	{
-		session_start();
-		$_SESSION['code'] = $_POST['code'];
-	}
+if(isset($_POST['code']))
+{
+	session_start();
+	$_SESSION['code'] = $_POST['code'];
+}
     
-    if (isset($_POST['todas'])) {
-        header("Location: all.php");
-    }
-    if (isset($_POST['salvar']) || isset($_POST['sair'])) {
-        if (isset($_POST['code']) and strlen($_POST['code'])>0) {
-            $_POST['function'] = 'save';
-            $json = json_decode(sendPost($SERVER, $_POST),true);
-
-	// See if there's already a fast note with this name
-	$result = $mysql->query("select fnote_id from fastnote where fnote_name = '$_POST[code]'");
-	if(!$result->num_rows)
+if(isset($_POST['todas'])) 
+{
+	header("Location: all.php");
+}
+elseif(isset($_POST['salvar']))
+{
+	if(isset($_POST['code']) and strlen($_POST['code'])>0) 
 	{
-		// There is not so create a new fastnote
-		$result = $mysql->query("insert into fastnote(fnote_name) values('$_POST[code]')");
-		if(!$result)
+		// $_POST['function'] = 'save';
+		//$json = json_decode(sendPost($SERVER, $_POST),true);
+
+		// See if there's already a fast note with this name
+		$result = $mysql->query("select fnote_id from fastnote where fnote_name = '$_POST[code]'");
+		if(!$result->num_rows)
 		{
-			die("Insert failed: " . mysql_error());
+			// There is not so create a new fastnote
+			$result = $mysql->query("insert into fastnote(fnote_name) values('$_POST[code]')");
+			if(!$result)
+			{
+				die("Insert failed: " . mysql_error());
+			}
+
+			$fnote_id = $mysql->insert_id;
+		}
+		else
+		{
+			$obj = $result->fetch_object();
+			$fnote_id = $obj->fnote_id;
 		}
 
-		$fnote_id = $mysql->insert_id;
+		// Now insert the line
+		$result = $mysql->query("select max(fnote_seq) as fnseq from fastnote_lines where fnote_id = $fnote_id");
+		if($result->num_rows)
+		{
+			$obj = $result->fetch_object();
+			$fnote_seq = $obj->fnseq;
+		}
+		else
+		{
+			$fnote_seq = 0;
+		}
+
+		++$fnote_seq;
+
+		$mysql->query("insert into fastnote_lines(fnote_id, fnote_seq, fnote_text) values($fnote_id, $fnote_seq, '$_POST[content]')");
+		echo '<html>
+		    <head>
+			<link href="style.css" type="text/css" rel="stylesheet">
+				<link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+				<meta name="viewport" content="width=device-width, initial-scale=0.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
+		   </head>
+		   <body>
+			    <a id="salvo" href="notpad.php"><div id="textCenter">SAVED</div></a>
+		   </body>
+		</html>';
 	}
-	else
+}
+elseif(isset($_POST['sair']))
+{
+	// Exit - see if the text is the same as the last one in the db...
+	$result = $mysql->query("select FL.fnote_id as fnote_id, FL.fnote_text as fnote_text, FL.fnote_seq as fnote_seq from fastnote_lines FL, fastnote F where F.fnote_name = '" . $_POST['code'] . "' and FL.fnote_id = F.fnote_id and FL.fnote_seq = (select max(fnote_seq) from fastnote_line where fnore_id = F.fnote_id)");
+	if($result)
 	{
 		$obj = $result->fetch_object();
-		$fnote_id = $obj->fnote_id;
-	}
+		if(strcmp($_POST['content'], $obj->fnote_text))
+		{
+			// Strings differ - for now just add the new row into the db
+			// Jeremy - you need to add the pop-up asking if they want to save
+			$seq = $obj->fnote_seq;
+			++$seq;
+			$mysql->query("insert into fastnote_lines(fnote_id, fnote_seq, fnote_text) values($obj->fnote_id, $seq, '$_POST[content]')");
+		}
+	}	
 
-
-	// Now insert the line
-	if($result = $mysql->query("select max(fnote_seq) as fnseq from fastnote_lines where fnote_id = $fnote_id"))
-	{
-		$obj = $result->fetch_object();
-		$fnote_seq = $obj->fnseq;
-	}
-	else
-	{
-		$fnote_seq = 0;
-	}
-
-	++$fnote_seq;
-	
-	$mysql->query("insert into fastnote_lines(fnote_id, fnote_seq, fnote_text) values($fnote_id, $fnote_seq, '$_POST[content]')");
-		
-            if (isset($_POST['sair'])) {
-		session_destroy();
-                header("Location: .");
-            } else {
-                $_SESSION['salvo'] = 1;
-                header("Location: notpad.php");
-            }
-        }
-    }
-?>
-<html>
-    <head>
-        <link href="style.css" type="text/css" rel="stylesheet">
-		<link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
-		<meta name="viewport" content="width=device-width, initial-scale=0.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
-<?php
-    echo "<a id='salvo' href='notpad.php'><div id='textCenter'>SALVO</div></a>";
+	session_destroy();
+	header("Location: .");
+}
 ?>
