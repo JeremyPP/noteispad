881ac51b082c16558d7f372e9f4eba5504395d7c
JeremyPP <jeremy.petit@ccc.ufcg.edu.br>
2014-05-09 22:24:06 -0300

    custom page 'Error 404' added

---
+++ 404.php
@@ -0,0 +1,29 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"
+		"http://www.w3.org/TR/html4/strict.dtd">
+<html lang="pt-br">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<link href="style.css" type="text/css" rel="stylesheet">
+<title>Error 404 - Page not found!</title>
+<link href="style.css" type="text/css" rel="stylesheet">
+<meta name="viewport" content="width=device-width, initial-scale=0.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
+<link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+<style>
+body{background:#4CAED3;-webkit-touch-callout:none;-webkit-user-select:none;-khtml-user-select:none;-moz-user-select:none;-ms-user-select:none;user-select:none}#i{height:122px;width:334px;position:absolute;bottom:0;margin-left:-166px}#a{width:750px;height:400px;position:absolute;top:0;bottom:0;left:0;right:0;margin:auto;font-family:'Open Sans',sans-serif;font-weight:900;color:#fff}#a h1{font-size:200px;margin-top:-10px;color:rgba(35,151,196,.66)}#a h2{margin-top:-1px;font-weight:300;max-width:400px;opacity:.3}#a h3{font-size:50px;margin-bottom:-10;margin-top:0;font-weight:900;text-transform:uppercase;color:#fff;opacity:.7}
+</style>
+</head>
+<body>
+<a href="http://notispad.com" style=" text-decoration: none; ">
+<div id="openb2">
+<img src="back.png">Back</div>
+</a>
+<center>
+<div id="a">
+<h3>Page not found!</h3>
+<h1>404</h1>
+<h2>There's that guy that said that thing about never being truly lost...</h2>
+</div>
+<img id="i" src="error_img.png"></img>
+</center>
+</body>
+</html>
