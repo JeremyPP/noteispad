58149b5605341f7932df5428e9ec10c5b1abdac3
Keith Jones <keith@pbembid.com>
2014-05-16 16:12:26 +0100

    Small script to log off a user (unsets user_id)

---
+++ logoff.php
@@ -0,0 +1,16 @@
+﻿<?php 
+require_once("init.php");
+require_once("functions.php");
+$SERVER = "http://localhost:8888";
+
+saveNote($_POST);
+
+session_start();
+
+error_log(">>>Before: " . $_SESSION['user_id']);
+unset($_SESSION['user_id']);
+error_log("<<<After: " . $_SESSION['user_id']);
+
+session_destroy();
+header("Location: .");
+?>
