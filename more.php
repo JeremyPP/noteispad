62edc15f5f05c5892883b5dfbc690cae9aff38dc
JeremyPP <jeremy.petit@ccc.ufcg.edu.br>
2014-05-20 13:05:04 -0300

    new message page if all notes have been used

---
+++ more.php
@@ -0,0 +1,25 @@
+﻿<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"
+		"http://www.w3.org/TR/html4/strict.dtd">
+<html lang="pt-br">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+		<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,600,300' rel='stylesheet' type='text/css'>
+		<link href="style.css" type="text/css" rel="stylesheet">
+		<link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+		<title>NOT is PAD!</title>
+		<meta name="viewport" content="width=device-width, initial-scale=0.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
+		<script src="http://code.jquery.com/jquery-2.1.0.min.js"></script>
+    </head>
+	<body class="moreNeP">
+		<h1>Ooops! ;)</h1>
+		<p id="morenoteText0">It seems that you have already used all your notes this month.</p>
+		<p>You will get 20 new ones in <b>14</b> days <b>5</b> hours and <b>37</b> minutes...</p>
+		<p id="morenoteText01">Until then, all the notes you create will be treated like Fast Notes, and will only
+			remain on our servers for 7 days. Also, you won't be able to see them in your note manager.</p>
+		<p id="morenoteText02"><b>Do you like to upgrade your plan to get more notes?</b></p>
+		<center>
+			<a href="config.php"><div class="confMoreN">Yes, take me to my settings page</div></a>
+			<a href="#"><div class="confMoreN" id="fastNConf">No, continue using Fast Notes</div></a>
+		</center>
+	</body>
+</html>
