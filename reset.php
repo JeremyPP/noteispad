2499d1e5e36962917de89106093d75dc69f6b5bb
JeremyPP <jeremy.petit@ccc.ufcg.edu.br>
2014-06-04 17:12:42 -0300

    some changes to the rest.php page

--- reset.php
+++ reset.php
@@ -14,86 +14,48 @@
 				padding:0;
 			}
 			body{
-				background: #4CAED3;
-			}
-			#title_main{
-				font-family: 'Open Sans', sans-serif;
-				color: #FFFFFF;
-				text-align: center;
-				font-size: 80px;
-				font-weight: 800;
-				padding-top: 100px;
-				padding-bottom: 50px;
-			}
-			#desc{
-				font-family: 'Open Sans', sans-serif;
-				color: #FFFFFF;
-				text-align: center;
-				font-size: 25px;
-				font-weight: 800;
-				margin-top: -20px;
-				padding-left: 30px;
-				padding-right: 30px;
-			}
-			#buttom_conf{
-				height: 50px;
-				border: none;
-				background: rgb(52, 128, 172);
-				color: #eee;
-				font-family: 'Open Sans', sans-serif;
-				font-weight: 900;
-				font-size: 22px;
-				border-radius: 3px;
-				margin-top: 70px;
-				padding-left: 20px;
-				padding-right: 20px;
-				-webkit-transition: all 200ms ease-in-out;
-				-moz-transition: all 200ms ease-in-out;
-				-ms-transition: all 200ms ease-in-out;
-				-o-transition: all 200ms ease-in-out;
-				transition: all 200ms ease-in-out;
-			}
-			#buttom_conf:hover{
-				background: #4C91BE;
-				cursor:pointer;
+				background: #4caed3;
 			}
 			#pass_bg{
 				position: fixed;
-				background: rgba(0, 0, 0, 0.7);
 				width: 100%;
 				height: 100%;
-				display:none;
 			}
 			#pass{
 				position: absolute;
-				background: #fff;
+				background: #eee;
 				width: 400px;
-				height: 100px;
-				line-height: 100px;
-				padding: 20px;
+				height: 200px;
+				padding: 30px;
 				top: 0;
 				bottom: 0;
 				right: 0;
 				left: 0;
 				margin: auto;
-				border-radius: 3px;
+				border-radius: 5px;
 				text-align: center;
-				color: #4CAED3;
+				color: #949494;
 				font-family: 'Open Sans', sans-serif;
 				font-weight: 900;
-				font-size: 20px;
+				font-size: 22px;
+				border-top: 5px solid rgb(85, 117, 160);
+				border-top-left-radius: 0;
+				border-top-right-radius: 0;
 			}
-			#pass span{
-				color: rgb(52, 128, 172);
+			#pass p {
+				max-width: 340px;
+				margin: 0 auto;
 			}
-			
-			#recoverfail {
+			#recoverfail, #recoverok {
 				background: #e74c3c;
 				height: 100%;
 				width: 100%;
 				position: absolute;
 				z-index: 999;
 				display: none;
+			}
+			#recoverok{
+				background: #4CAED3;
 			}
 			.recovery-center {
 				top: 0;
@@ -130,7 +92,7 @@
 				font-size: 18px;
 				cursor: pointer;
 			}
-			#recoverfail h1 {
+			#recoverfail h1, #recoverok h1 {
 				font-family: 'Open Sans', sans-serif;
 				color: #FFFFFF;
 				text-align: center;
@@ -138,11 +100,51 @@
 				font-weight: 800;
 				margin-top: 30px;
 			}
+			
+			#respass{
+				width: 350px;
+				height: 50px;
+				border: none;
+				background: #f9f9f9;
+				padding-left: 10px;
+				color: #3d464d;
+				border-radius: 3px;
+				outline: none;
+				font-size: 18px;
+				margin-top: 25px;
+			}
+			#respass:focus{
+				background: #fff;
+			}
+			#submitpass{
+				width: 140px;
+				height: 40px;
+				background: #75B0C7;
+				color: #FFFFFF;
+				border-radius: 3px;
+				outline: none;
+				font-size: 18px;
+				margin-top: 25px;
+				border: none;
+				-webkit-transition: all 200ms ease-in-out;
+				-moz-transition: all 200ms ease-in-out;
+				-ms-transition: all 200ms ease-in-out;
+				-o-transition: all 200ms ease-in-out;
+				transition: all 200ms ease-in-out;
+			}
+			#submitpass:hover{
+				background: #5D93A8;
+				cursor: pointer;
+			}
 		</style>
     </head>
         <body>
 		<div id="pass_bg">
-			<div id="pass">Your new password is: <span>7c52DA9s</span></div>
+			<div id="pass">
+				<p>Please, type your new password below:</p>
+				<input id="respass" autofocus="1" type="password" name="pass" placeholder="Your new password" required="">
+				<input id="submitpass" type="submit" value="Reset" onclick="pass()"></input>
+			</div>
 		</div>
 		<div id="recoverfail">
 			<div class="recovery-center">
@@ -153,18 +155,17 @@
 				<a href="http://pbembid.com:8888/forgot.php" style="text-decoration: none;"><div id="voltar-pass-er-b">Ok</div></a>
 			</div>
 		</div>
-		<h1 id="title_main">Hello Keith</h1>
-		<p id="desc">Click on the button below to generate a new random password for your account.<br>
-		Don't forget to change it once logged by going to Settings > Change Password.</p>
-		<center>
-			<input id="buttom_conf" type="button" value="Generate new password" onclick="pass()"></input>
-		</center>
+		<div id="recoverok">
+			<div class="recovery-center">
+				<h1>Thank you! :)</h1>
+				<h2>
+					Your password have been successfully reset, from now on you can log in to your account with your new password.
+				</h2>
+			</div>
+		</div>
 		<script>
 		function pass(){
-			$("#pass_bg").show();
-			$("#title_main").hide();
-			$("#desc").hide();
-			$("#buttom_conf").hide();
+			$("#recoverok").show();
 		}
 		function expir(){
 			$("#recoverfail").show();
