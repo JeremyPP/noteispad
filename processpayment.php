947e62ecf80ca013b1af57ad80818cd7649525d3
Keith Jones <keith@pbembid.com>
2014-06-15 16:07:06 +0100

    Process various PayPal payment messages

---
+++ processpayment.php
@@ -0,0 +1,29 @@
+<?php
+require_once('functions.php');
+require_once('PPfunctions.php');
+session_start();
+
+if(isset($_REQUEST['token']))
+{
+	if(isset($_REQUEST['PayerID']))
+	{	
+		$_SESSION['pp_token'] = $_REQUEST['token'];
+		$_SESSION['pp_payerid'] = $_REQUEST['PayerID'];
+		$res = CreateRecurringPaymentsProfile($_REQUEST['token']);
+		if(isset($res['ACK']) && ($res['ACK'] == 'SUCCESS'))
+		{
+			$id = addUser($_SESSION['preauth_name'], $_SESSION['preauth_password'], $_SESSION['preauth_email'], $_SESSION['preauth_planno'], $_SESSION['pp_payerid'], $_SESSION['preauth_date']);
+			$_SESSION['user_id'] = $id;
+			if(isset($_SESSION['remember_me']))
+			{
+				addAuth($id);
+			}
+
+			header("Location: me.php");
+			exit();
+		}
+	}
+}
+
+header("Location: index.php");
+?>
