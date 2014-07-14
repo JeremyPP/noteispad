d86a5597c5a2b5374fcbb7e2f6d5e20071c3e803
JeremyPP <jeremy.petit@ccc.ufcg.edu.br>
2014-07-14 15:31:14 -0300

    change error page (404.php)

--- 404.php
+++ 404.php
@@ -3,27 +3,22 @@
 <html lang="en">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<link href="style.css" type="text/css" rel="stylesheet">
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,600,300' rel='stylesheet' type='text/css'>
 <title>Error 404 - Page not found!</title>
-<link href="style.css" type="text/css" rel="stylesheet">
 <meta name="viewport" content="width=device-width, initial-scale=0.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
 <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
 <style>
-body{background:#4CAED3;-webkit-touch-callout:none;-webkit-user-select:none;-khtml-user-select:none;-moz-user-select:none;-ms-user-select:none;user-select:none}#i{height:122px;width:334px;position:absolute;bottom:0;margin-left:-166px}#a{width:750px;height:400px;position:absolute;top:0;bottom:0;left:0;right:0;margin:auto;font-family:'Open Sans',sans-serif;font-weight:900;color:#fff}#a h1{font-size:200px;margin-top:-10px;color:rgba(35,151,196,.66)}#a h2{margin-top:-1px;font-weight:300;max-width:400px;opacity:.3}#a h3{font-size:50px;margin-bottom:-10;margin-top:0;font-weight:900;text-transform:uppercase;color:#fff;opacity:.7}
+body{background:#D34C4C;-webkit-touch-callout:none;-webkit-user-select:none;-khtml-user-select:none;-moz-user-select:none;-ms-user-select:none;user-select:none}a{text-decoration:none;color:#D8D8D8;}#a{width:750px;height:430px;position:absolute;top:0;bottom:0;left:0;right:0;margin:auto;font-family:'Open Sans',sans-serif;font-weight:900;color:#fff}#a h1{font-size:200px;margin-top:-45px;color:#AC4343;margin-bottom: 0px;}#a h2{margin-top:-1px;font-weight:700;max-width:420px;opacity:.3}#a h3{font-size:50px;margin-bottom:15px;margin-top:-35px;font-weight:900;text-transform:uppercase;color:#fff;opacity:.7}#b{background: #333; height: 40px; width: 170px; line-height: 40px; border-radius: 20px; margin-top: 25px; font-size: 18px; color: #D8D8D8; font-weight: 700; cursor: pointer;}
 </style>
 </head>
 <body>
-<a href="http://notispad.com" style=" text-decoration: none; ">
-<div id="openb2">
-<img src="back.png">Back</div>
-</a>
 <center>
 <div id="a">
+<h1>404.</h1>
 <h3>Page not found!</h3>
-<h1>404</h1>
 <h2>There's that guy that said that thing about never being truly lost...</h2>
+<a href="http://notispad.com"><div id="b">Go back home</div></a>
 </div>
-<img id="i" src="error_img.png"></img>
 </center>
 </body>
 </html>
