6c622f3e46bb5e35e9e614e24eb1c38e6deb6709
JeremyPP <jeremy.petit@ccc.ufcg.edu.br>
2014-08-21 20:03:54 -0300

    add Google Analytics script

--- me.php
+++ me.php
@@ -64,8 +64,14 @@ elseif(failedPayment($_SESSION['user_id']))
 		<link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
 		<script src="http://code.jquery.com/jquery-2.1.0.min.js"></script>
 		<script src="erro.js"></script>
-        <script type="text/javascript">
-        </script>
+        <script>
+		  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+		  ga('create', 'UA-54091012-1', 'auto');
+		  ga('send', 'pageview');
+		</script>
     </head>
         <body>			
 <?php
